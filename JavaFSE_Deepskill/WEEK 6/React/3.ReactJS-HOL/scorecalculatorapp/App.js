dcc2945e5b1cb74118164cb33a7eb6390850b2f9
Lisa04-code <lisachakraborty020@gmail.com>
2025-07-24 16:12:51 +0530

    Add files via upload

---
+++ JavaFSE_Deepskill/WEEK 6/React/3.ReactJS-HOL/scorecalculatorapp/App.js
@@ -0,0 +1,18 @@
+import {CalculateScore} from'../src/Components/CalculateScore';
+
+function App()
+{
+return(
+<div>
+    <CalculateScore Name={"Ahaan"}
+   School={"DNV Public School"}
+   total={284}
+   goal={3}
+  />
+</div>
+)
+}
+
+export default App;
+
+
