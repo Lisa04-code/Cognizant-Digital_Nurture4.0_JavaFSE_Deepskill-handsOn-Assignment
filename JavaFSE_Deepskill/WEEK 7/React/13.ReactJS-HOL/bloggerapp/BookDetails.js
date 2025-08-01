d94bfe0204d3f9daab3d35a84596045cc0a5c5fe
Lisa04-code <lisachakraborty020@gmail.com>
2025-08-01 07:53:10 +0530

    Update BookDetails.js

--- JavaFSE_Deepskill/WEEK 7/React/13.ReactJS-HOL/bloggerapp/BookDetails.js
+++ JavaFSE_Deepskill/WEEK 7/React/13.ReactJS-HOL/bloggerapp/BookDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BookDetails = () => {
   return (
     <div>
-      <h2>📚 Book Details</h2>
+      <h2> Book Details</h2>
       <p>Title: The Alchemist</p>
       <p>Author: Paulo Coelho</p>
     </div>
