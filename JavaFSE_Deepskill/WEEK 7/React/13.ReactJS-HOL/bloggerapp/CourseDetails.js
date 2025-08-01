f5a6ec2b3f9cf31579565b0aae5774befbe2d073
Lisa04-code <lisachakraborty020@gmail.com>
2025-08-01 07:53:24 +0530

    Update CourseDetails.js

--- JavaFSE_Deepskill/WEEK 7/React/13.ReactJS-HOL/bloggerapp/CourseDetails.js
+++ JavaFSE_Deepskill/WEEK 7/React/13.ReactJS-HOL/bloggerapp/CourseDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CourseDetails = () => {
   return (
     <div>
-      <h2>🎓 Course Details</h2>
+      <h2> Course Details</h2>
       <p>Course: React for Beginners</p>
       <p>Instructor: John Doe</p>
     </div>
